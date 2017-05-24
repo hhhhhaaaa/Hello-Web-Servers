f6b55d1dd5a8aa0c7010007c0916c3865672a9d4
Doug L <douglubaway@gmail.com>
2017-05-24 09:38:10 -0700

    Was able to sort the files alphabetically

--- server.js
+++ server.js
@@ -2,6 +2,9 @@
 var express = require('express');
 var path = require('path');
 var fs = require("fs");
+var albums = app.locals.albums = require("./public/albums.json");
+var artists = app.locals.artists = require("./public/artists.json");
+var songs = app.locals.songs = require("./public/songs.json");
 
 var app = express();
 
@@ -22,26 +25,58 @@ app.get('/songs', function(req, res) {
   res.render('songs');
 });
 
+app.get('/albums/:album_id', function(req, res) {
+  res.render('album');
+});
+
+app.get('/artists/:artist_id', function(req, res) {
+  res.render('artist');
+});
+
+//Used to assign the pages to variables
+//app.params.album_id = albums;
+//app.params.artist_id = artists;
 
 //Used to grab the static files
 app.use(express.static(path.join(__dirname + 'public')));
 
-var albums = app.locals.albums = JSON.stringify(require("./public/albums.json"));
-var artists = app.locals.artists = JSON.stringify(require("./public/artists.json"));
-var songs = app.locals.songs = JSON.stringify(require("./public/songs.json"));
+var albumsSorted = [];
+var artistsSorted = [];
+var songsSorted = [];
+//Albums
+for (var properties in albums) {
+  albumsSorted.push(albums[properties])
+}
+//Artists
+for (var properties in artists) {
+  artistsSorted.push(artists[properties])
+}
+//Songs
+for (var properties in songs) {
+  songsSorted.push(songs[properties])
+}
 
 //Used to arrange the JSON files
-albums.sort(function(a, b) {
-    return parseFloat(a.title) - parseFloat(b.title);
+albumsSorted.sort(function(a, b) {
+  var x = a.title.toLowerCase();
+  var y = b.title.toLowerCase();
+//If x is less than y, move it down; else if x is greater than y, move it up; else keep it where it is.
+  return x < y ? -1 : x > y ? 1 : 0;
 });
 
-artists.sort(function(a, b) {
-    return parseFloat(a.name) - parseFloat(b.name);
+artistsSorted.sort(function(a, b) {
+  var x = a.name.toLowerCase();
+  var y = b.name.toLowerCase();
+  return x < y ? -1 : x > y ? 1 : 0;
 });
 
-songs.sort(function(a, b) {
-    return parseFloat(a.title) - parseFloat(b.title);
+songsSorted.sort(function(a, b) {
+  var x = a.title.toLowerCase();
+  var y = b.title.toLowerCase();
+  return x < y ? -1 : x > y ? 1 : 0;
 });
+
+console.log(albums[0].artist_id)
 
 //Used to display the page.
 app.listen(3000, function() {
